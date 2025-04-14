4e44bc088e4d9b58f7f0168783418672938df9dd
Nouman <168581465+Nouman-wp@users.noreply.github.com>
2025-04-14 23:27:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+router.post('/chat', (req, res) => {
+  const { username } = req.body;
+  if (!username || username.trim() === '') {
+    return res.redirect('/');
+  }
+  res.render('chat', { username });
+});
+
+module.exports = router;
