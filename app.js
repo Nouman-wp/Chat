d64d21059c7db2ff66def2f39ed75bcc556b65a7
Nouman <168581465+Nouman-wp@users.noreply.github.com>
2025-04-22 21:38:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose');
 const io = require('socket.io')(http);
 const path = require('path');
 
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/User');
+
 mongoose.connect('mongodb://127.0.0.1:27017/chatApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
