df9b440327e0ea9c80ccaf28ef6e249c15dc685c
Nouman <168581465+Nouman-wp@users.noreply.github.com>
2025-04-23 23:31:52 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,8 @@
-middleware.js
+
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash('error', 'You must be signed in to access the chat.');
+    return res.redirect('/login');
+  }
+  next();
+};
